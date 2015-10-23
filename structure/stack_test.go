de7203347eb4d1ef5a2ccaad70a33a2e46ffdefe
Aiden Luo <aiden0xz@gmail.com>
2015-10-23 22:25:03 +0800

    add test case for stack

---
+++ structure/stack_test.go
@@ -0,0 +1,25 @@
+package structure
+
+import (
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	emptyStack := NewStack(0)
+	if _, err := emptyStack.Pop(); err != ErrEmptyStack {
+		t.Errorf("Pop from empty stack not return error %v", err)
+	}
+	fullStack := NewStack(2)
+	if fullStack.Len() != 0 {
+		t.Errorf("Initialize stack error")
+	}
+	fullStack.Push(1)
+	fullStack.Push(2)
+	value, err := fullStack.Pop()
+	if err != nil {
+		t.Errorf("Pop from stack return error %v", err)
+	}
+	if value != 2 {
+		t.Errorf("Value pop from stack not equal to which pushed into")
+	}
+}
